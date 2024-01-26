bcc741ba2ad3e6cdef1f93ccd57e0dad831e4ef1
PhatCM <phat.cm@trans-tech.vn>
2024-01-26 11:10:39 +0700

    update slide makeup. Done concepts

--- js/concept.js
+++ js/concept.js
@@ -20,7 +20,7 @@
                     $.each(concepts, function (index, item) {
                         let concept = '<div class=" item">' +
                             // '<div class="numbertext">' + (index + 1) + '/' + concepts.length + '</div>' +
-                            '<img src="https://images.unsplash.com/photo-1519681393784-d120267933ba?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8MTkyMHgxMDgwfGVufDB8fDB8fHww">' +
+                            '<img src="'+ item.image_url +'">' +
                             // '<img src="' + item.image_url + '">' +
                             '<div class="mySlideText"><span class="concept-title">' + item.name + '</span><p>' + item.description + '</p></div>' +
                             '</div>'
@@ -29,7 +29,6 @@
                     });
 
                     var owl = $("#testing");
-
 
                     owl.owlCarousel({
                         loop:true,
